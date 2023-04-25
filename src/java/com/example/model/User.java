89c22656eedce2b24be6d4a7c8c2901e3d8fb997
subramanyam123-c <72187961+subramanyam123-c@users.noreply.github.com>
2023-04-25 12:26:22 -0500

    Add files via upload
    made few changes in user model class.

--- src/java/com/example/model/User.java
+++ src/java/com/example/model/User.java
@@ -1,30 +1,41 @@
-package com.example.model;
+package com.qba.app.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users", uniqueConstraints={@UniqueConstraint(columnNames = {"email"})})
 public class User {
-    private String email;
-    private String password;
-    
-    public User() {
-    }
-    
-    public User(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String email;
+	private String username;
+	private String password;
+	private String firstname;
+	private String usertype;
+	private String lastname;
+	private String age;
+	private String mobileNumber;
+	private String doorNo;
+	private String street;
+	private String city;
+	private String zipCode;
+	private String isApproved;
+	private String securityQuestion;
+	private String securityAnswer;
+	
+
 }
