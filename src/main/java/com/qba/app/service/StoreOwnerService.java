9573fabc5d41dab499f5fd8bece977839c08d8d3
Adithya <kasarlaadithya642@gmail.com>
2023-04-28 00:53:04 -0500

    store sontroller is added

---
+++ src/main/java/com/qba/app/service/StoreOwnerService.java
@@ -0,0 +1,39 @@
+package com.qba.app.service;
+
+import java.util.List;
+
+import com.qba.app.model.Category;
+import com.qba.app.model.Item;
+import com.qba.app.model.Store;
+
+public interface StoreOwnerService {
+
+	List<Store> getAllStores();
+
+	void saveStore(Store store);
+
+	void deleteStore(Long id);
+
+	List<Category> getAllCategories();
+
+	void saveCategory(Category category);
+
+	void deleteCategory(Long id);
+
+	List<Item> getAllItems();
+
+	void saveItem(Item item);
+
+	void deleteItem(Long id);
+
+	Item getItemById(Long id);
+
+	void updateItem(Item item);
+
+	Category getCategoryById(Long id);
+
+	void updateCategory(Category category);
+
+	void updateOrderStatus(String status, Long id);
+
+}
