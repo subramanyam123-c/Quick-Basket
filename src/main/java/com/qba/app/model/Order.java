6bce98bbacc1436940804b425baef6d5546a42fb
Subbu <subramanyamb30@gmail.com>
2023-04-28 23:46:46 -0500

    Added orders,request and review model classes

---
+++ src/main/java/com/qba/app/model/Order.java
@@ -0,0 +1,38 @@
+package com.qba.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String name;
+	private String email;
+	private String price;
+	private String quantity;
+	private String discount;
+	private String totalCost;
+	private String finalBill;
+	private String cardName;
+	private String cardNumber;
+	private String cvv;
+	private String status;
+
+}
